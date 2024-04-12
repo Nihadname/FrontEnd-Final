439111607ce72b50770790e35683a03a35c461d8
Nihadname <nihadcoding@gmail.com>
2024-04-13 01:49:26 +0400

    all what Ihave done is the whole of the website

--- assets/js/app.js
+++ assets/js/app.js
@@ -79,6 +79,59 @@ $(".titleCar").click(function(){
     margin-right: auto;">View All  Brands</span>
 </div>`)
 })    
+$(".second").click(function(){
+    $(".DivCar").html(` <div class="itemsInIt">
+                                <img src="./assets/img/ray-ban.webp" alt="">
+                                <span class="carName">Ray-Ban</span>
+                            </div>
+                            <div class="itemsInIt">
+                                <img src="./assets/img/zeiss.webp" alt="">
+                                <span class="carName">Zeiss</span>
+                            </div>
+                            <div class="itemsInIt">
+                                <img src="./assets/img/occular.webp" alt="">
+                                <span class="carName">Ocullar</span>
+                            </div>
+                            <div class="itemsInIt">
+                                <img src="./assets/img/apple.webp" alt="">
+                                <span class="carName">Apple</span>
+                            </div>
+                            <div class="itemsInIt mb-5 ">
+                                <img src="./assets/img/titan.webp"  alt="">
+                                <span class="carName">Titan</span>
+                            </div>
+                          
+                            <div class="itemsInIt">
+                                <span class="carName text-center " style="    margin-left: auto;
+                                margin-right: auto;">View All  Brands</span>
+                            </div>`)
+})
+
+$(".second2").click(function(){
+    $(".DivCar").html(`
+    <div class="itemsInIt">
+        <img src="./assets/img/herman miller.webp" alt="">
+        <span class="carName">Scarlett Beauty </span>
+    </div>
+    <div class="itemsInIt">
+        <img src="./assets/img/otobi.webp" alt="">
+        <span class="carName">Scroll Through</span>
+    </div>
+    <div class="itemsInIt">
+        <img src="./assets/img/hatil.webp" alt="">
+        <span class="carName">Coveted Clicks</span>
+    </div>
+    <div class="itemsInIt mb-5 ">
+        <img src="./assets/img/steelcase.webp" alt="">
+        <span class="carName">Constant Shoppers</span>
+    </div>
+    <div class="itemsInIt">
+<span class="carName text-center " style="    margin-left: auto;
+margin-right: auto;">View All  Brands</span>
+</div>
+
+`);
+})
 });
 
 
